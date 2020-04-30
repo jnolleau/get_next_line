6e6159e17b652ba634ceccc875dd76991c128415
Sequoiageant <j.nolleau88@gmail.com>
2020-04-30 15:14:36 +0200

    some refactoring

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: julnolle <julnolle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 14:52:50 by julnolle          #+#    #+#             */
-/*   Updated: 2020/04/30 12:03:49 by julnolle         ###   ########.fr       */
+/*   Updated: 2020/04/30 15:10:11 by julnolle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,6 @@ size_t			ft_strlen(const char *str)
 	while (str[i] != '\0')
 		i++;
 	return (i);
-}
-
-char			*ft_strjoin(char const *s1, char const *s2)
-{
-	char		*dst;
-	size_t		size;
-	size_t		i;
-	size_t		j;
-
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	size = ft_strlen(s1) + ft_strlen(s2);
-	if (!(dst = (char *)malloc(sizeof(char) * (size + 1))))
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		dst[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		dst[i + j] = s2[j];
-		j++;
-	}
-	dst[i + j] = '\0';
-	return (dst);
 }
 
 static	void	*ft_memcpy(char *dst, const char *src, size_t n)
@@ -70,6 +42,7 @@ char			*ft_strdup(const char *src)
 	char	*dest;
 	size_t	len;
 
+	dest = NULL;
 	len = ft_strlen(src);
 	dest = (char *)malloc(sizeof(char) * (len + 1));
 	if (dest != NULL)
@@ -77,12 +50,34 @@ char			*ft_strdup(const char *src)
 	return (dest);
 }
 
+char			*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dst;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		dst = ft_strdup(s2);
+	else if (!s2)
+		dst = ft_strdup(s1);
+	else
+	{
+		len_s1 = ft_strlen(s1);
+		len_s2 = ft_strlen(s2);
+		if (!(dst = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1))))
+			return (NULL);
+		ft_memcpy(dst, s1, len_s1);
+		ft_memcpy(dst + len_s1, s2, len_s2 + 1);
+	}
+	return (dst);
+}
+
 char			*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*dst;
-	size_t	i;
 
-	i = 0;
 	dst = NULL;
 	dst = (char *)malloc(sizeof(char) * (len + 1));
 	if (s == NULL || dst == NULL)
@@ -91,13 +86,8 @@ char			*ft_substr(char const *s, unsigned int start, size_t len)
 		dst[0] = '\0';
 	else
 	{
-		while (s[start] != '\0' && i < len)
-		{
-			dst[i] = s[start];
-			i++;
-			start++;
-		}
-		dst[i] = '\0';
+		dst = ft_memcpy(dst, s + start, len);
+		dst[len] = '\0';
 	}
 	return (dst);
 }
